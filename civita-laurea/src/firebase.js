198a581e7bcd63f5b7c8d0171a7874456232b810
Mason Garrett <thegarretts99@gmail.com>
2021-03-21 19:32:22 -0400

    Setup React App
    Created React App and connected to firebase.

---
+++ civita-laurea/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBXX6jAB3MR1lEwdEoAekQAmiJTddGkp_A",
+    authDomain: "civita-laurea.firebaseapp.com",
+    projectId: "civita-laurea",
+    storageBucket: "civita-laurea.appspot.com",
+    messagingSenderId: "936720196161",
+    appId: "1:936720196161:web:263c480867dafebcb49cc0",
+    measurementId: "G-E07JC0TZTX"
+  };
