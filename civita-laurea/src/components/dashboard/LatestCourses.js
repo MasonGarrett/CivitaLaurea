860747ea433ead1090b66d55bc79725ce1026b3d
Mason Garrett <thegarretts99@gmail.com>
2021-04-15 11:23:12 -0400

    Updated Dashboard

--- civita-laurea/src/components/dashboard/LatestCourses.js
+++ civita-laurea/src/components/dashboard/LatestCourses.js
@@ -10,49 +10,80 @@ import {
   Divider,
 } from '@material-ui/core';
 import ArrowRightIcon from '@material-ui/icons/ArrowRight';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
 import ProductCard from '../product/ProductCard';
 import products from '../../__mocks__/products';
+import { db } from '../../firebase';
+import { selectUser } from '../../features/userSlice';
 
-const LatestOrders = (props) => (
-  <Card {...props}>
-    <CardHeader title="Latest Courses" />
-    <Divider />
-    <PerfectScrollbar>
-      <Box sx={{ minWidth: 800 }}>
-        <Container maxWidth={false}>
-          <Box sx={{ pt: 3 }}>
-            <Grid container spacing={3}>
-              {products.map((product) => (
-                <Grid item key={product.id} lg={4} md={6} xs={12}>
-                  <ProductCard
-                    product={product}
-                    component={RouterLink}
-                    to="/app/course"
-                  />
-                </Grid>
-              ))}
-            </Grid>
-          </Box>
-        </Container>
-      </Box>
-    </PerfectScrollbar>
-    <Box
-      sx={{
-        display: 'flex',
-        justifyContent: 'flex-end',
-        p: 2,
-      }}
-    >
-      <Button
-        color="primary"
-        endIcon={<ArrowRightIcon />}
-        size="small"
-        variant="text"
+function LatestOrders() {
+  const user = useSelector(selectUser);
+  const [courses, setCourses] = useState([]);
+
+  const fetchUser = async () => {
+    const response = db.collection('users');
+    const data = await response.get();
+    data.docs.forEach((userr) => {
+      if (userr.id === user.uid) {
+        const userCourses = userr.data().courses;
+        for (let i = 0; i < userCourses.length; i += 1) {
+          db.collection('courses')
+            .doc(userCourses[i])
+            .get()
+            .then((docCourse) => {
+              setCourses((prev) => [...prev, ...courses, docCourse.data()]);
+            });
+        }
+      }
+    });
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <Card>
+      <CardHeader title="Latest Courses" />
+      <Divider />
+      <PerfectScrollbar>
+        <Box sx={{ minWidth: 800 }}>
+          <Container maxWidth={false}>
+            <Box sx={{ pt: 3 }}>
+              <Grid container spacing={3}>
+                {courses.map((course) => (
+                  <Grid item key={course.id} lg={4} md={6} xs={12}>
+                    <ProductCard
+                      product={course}
+                      component={RouterLink}
+                      to="/app/course"
+                    />
+                  </Grid>
+                ))}
+              </Grid>
+            </Box>
+          </Container>
+        </Box>
+      </PerfectScrollbar>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'flex-end',
+          p: 2,
+        }}
       >
-        View all
-      </Button>
-    </Box>
-  </Card>
-);
+        <Button
+          color="primary"
+          endIcon={<ArrowRightIcon />}
+          size="small"
+          variant="text"
+        >
+          View all
+        </Button>
+      </Box>
+    </Card>
+  );
+}
 
 export default LatestOrders;
